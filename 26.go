712862278b73f8a8383ab52255b22ba5c5209220
Zhenyuan Wang <zhenyuan_wang@foxmail.com>
2020-08-09 15:20:35 +0800

    update

--- 26.go
+++ 26.go
@@ -14,10 +14,21 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
 
-
 var c Celsius
 var f Fahrenheit
 fmt.Println(c == 0)          // "true"
 fmt.Println(f >= 0)          // "true"
 fmt.Println(c == f)          // compile error: type mismatch
 fmt.Println(c == Celsius(f)) // "true"!
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+
+// 许多类型都会定义一个String方法，因为当使用fmt包的打印方法时，将会优先使用该类型对应的String方法返回的结果打印
+
+c := FToC(212.0)
+fmt.Println(c.String()) // "100°C"
+fmt.Printf("%v\n", c)   // "100°C"; no need to call String explicitly
+fmt.Printf("%s\n", c)   // "100°C"
+fmt.Println(c)          // "100°C"
+fmt.Printf("%g\n", c)   // "100"; does not call String
+fmt.Println(float64(c)) // "100"; does not call String
